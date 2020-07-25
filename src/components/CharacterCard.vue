350649e988be555b26912edd15bc0ae64603b3ae
Mlipphardt <matthewlipphardt@gmail.com>
2020-07-25 03:19:08 -0400

    Initial commit

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="characterCard">
+    <div class="cost">
+      {{ cost }}
+    </div>
+    <div class="name"></div>
+    <div class="attack">{{ attack }}</div>
+    <div class="health">{{ health }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      attack: 2,
+      health: 1,
+      cost: 1,
+    };
+  },
+};
+</script>
+
+<style>
+.characterCard {
+  position: relative;
+  height: 205px;
+  width: 137px;
+  background-color: red;
+}
+
+.cost .attack .health {
+  background-color: white;
+}
+
+.cost {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+
+.attack {
+  position: absolute;
+  bottom: 5px;
+  left: 10px;
+}
+
+.health {
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="mx-auto">
+        <CharacterCard />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CharacterCard from "../components/CharacterCard";
+export default {
+  name: "index",
+  components: {
+    CharacterCard,
+  },
+  data: () => {},
+};
+</script>
+
+<style></style>
